dc1e7cbda49ef8775d4da93ddffdf8d0f9cb946f
holykid <zulfaqarfiery@gmail.com>
2023-05-05 23:08:53 +0800

    Initial Commit

---
+++ kidfit/urls.py
@@ -0,0 +1,30 @@
+"""kidfit URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from . import fuzzy
+
+    
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('accounts.urls')),
+    path('', TemplateView.as_view(template_name='base.html')),
+    path('size-conversion', TemplateView.as_view(template_name='size-conversion.html')),
+    path('size-charts', TemplateView.as_view(template_name='size-charts.html')),
+    path('output', fuzzy.output),
+
+]
